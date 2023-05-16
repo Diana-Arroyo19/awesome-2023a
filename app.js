773093124d125b7a5ac1c7de1ac820f8ce50baab
DIANA ARROYO <124737303+Diana-Arroyo19@users.noreply.github.com>
2023-05-16 14:38:21 +0000

    ✨✔Proceso del formulario

--- app.js
+++ app.js
@@ -1,47 +1,73 @@
-// Importar el modulo http
-import http from 'http';
-
-// importando express
+// Importando expressjs
 import express from 'express';
 
-// Crear instancia de express
-// crear constante
-const app = express();//(req,res)=> {MONTON DE CODIGO}
+// Crear una instancia de express
+const app = express(); // (req, res)=>{ UN MONTO DE CODIGO }
 
-// registarr mi primier middleware
+// middleware fr parseo de datos del cliente
+app.use(express.urlencoded({extended:true}));
+
+// Registrar nuestro primer middleware
 app.use((req, res, next) => {
-    console.log("⭐Ejecunatndo el Middleware 1");
-    // invocando el siguiente middleware
-    next();
-});
-
-
-app.use((req, res, next) => {
-    console.log(`🎆${req.method} - ${req.url}`);
-    // invocando el siguiente middleware
+    console.log("⭐ Ejecutando el Middleware 1");
+    // Invocando al siguiente middleware
     next();
 });
 
 app.use((req, res, next) => {
-    console.log("⭐Respondiendo al cliente");
+    console.log(`🪄 ${req.method} - ${req.url}`);
+    next();
+});
+
+app.use('/about', (req, res) => {
     res.send(`
-    <h1>Welcome to express</h1>
-    <p>This is my awesome app</p>`
-    );
+  <h1 style="color: teal">About...</h1>
+  <p style="color: #555"> Esta pagina creada para aprender 
+  desarrollo web en Fullstack con JS</p>
+  `);
 });
 
-// creacion del servidor
-// creacion de su constante
-const server = http.createServer(app);
+//GET /add-product
+app.get('/add-product', (req, res, next) => {
+    // sirviendo al formulario
+    console.log("Sirviendo el formulario");
+    res.send(`
+        <form action ="/add-product" method="POST">
+            <label for="title"> Title </label>
+            <input id = "title" type = "text" name = "title" style = "border-radius: 5px">
+            <label for="description"> Description </label>
+            <input id = "description" type = "text" name = "title" style = "border-radius: 5px">
+            <button type = "submit">Add Product</button>
+        </form>
+    `);
+});
 
-// definir puertos
+//POST /add-product
+app.post('/add-product', (req, res) =>{
+    // realizando extraccion de los dtaos de peticion
+    for(const prop in req.body){
+        console.log(`PROP: ${prop} : ${req.body[prop]}`);
+    };
+    res.redirect('/');
+});
+
+
+app.use((req, res) => {
+    console.log("⭐ Respondiendo al cliente");
+    res.send(`
+  <h1>Bienvenido a express</h1>
+  <p>Este es mi software</p>
+  `);
+});
+
+// Creando el servidor
+// const server = http.createServer(app);
+// Definir puertos
 const port = 3000;
 const ip = "0.0.0.0";
 
-// arrancar el servidor
-server.listen(port, ip, (err)=>{
+// Arrancando el server
+app.listen(port, ip, (err) => {
     console.log("📢 Sirviendo en http://localhost:3000");
-}); //escuchar peticiones
-
-
-
+    console.log(`📢 Sirviendo en http://${process.env.IP}:${process.env.PORT}`);
+});
