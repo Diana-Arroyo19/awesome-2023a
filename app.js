76f526de8ca45614a9efb66746a52f648efa7d5c
DIANA ARROYO <124737303+Diana-Arroyo19@users.noreply.github.com>
2023-05-26 15:48:05 +0000

    📂Servidor de archivos estaticos

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 // Importando expressjs
 import express from 'express';
 
-import httpStatus from 'http';
+// importando http-status
+import httpStatus from 'http-status';
+
+// importando path
+import path from 'path';
 
 // Importando el enrutador
 import adminRouter from './routes/admin.route.js';
-
 import shopRouter from './routes/shop.route.js';
-import path from 'path';
+
+// omportando ROOT_DIR
+import { ROOT_DIR } from './helpers/paths.js';
 
 // Crear una instancia de express
 const app = express(); // (req, res)=>{ UN MONTO DE CODIGO }
 
 // middleware fr parseo de datos del cliente
 app.use(express.urlencoded({extended:true}));
+
+// se resgistra el middleware para 
+// servidor de archivos estaticos
+app.use(express.static(path.join(ROOT_DIR,'public')));
 
 // Se agrega ruta de administrador
 app.use('/admin',adminRouter);
