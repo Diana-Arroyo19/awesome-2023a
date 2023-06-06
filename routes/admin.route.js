93867d5f923226c85d7190bdceb39b276f3a1f8e
DIANA ARROYO <124737303+Diana-Arroyo19@users.noreply.github.com>
2023-06-06 01:45:30 +0000

    ⛓ Patron MVC

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,42 +1,16 @@
 // Importando el enrutador de express
 import { Router } from 'express';
-
-// Importando el gestor de rutas
-import path from 'path';
-
-// importar ROOT_DIR
-import { ROOT_DIR } from '../helpers/paths.js';
+// Importando funciones del controlador
+import { getAddProduct, postAddProduct } from '../controllers/products.controller.js'
 
 // Creando una instancia del enrutador de express
 const router = Router();
 
-// creando datos en la memoria volatil
-export const products =[ ];
-
 // GET /admin/add-product
-router.get('/add-product', (req, res, next) => {
-  // Servimos el formulario
-  console.log(`ROOT_DIR: ${ROOT_DIR}`);
-  console.log("📢 Sirviendo formulario...");
-  // res.sendFile(path.join(ROOT_DIR, "views", "add-product.html"));
-  res.render('add-product', {
-    addProduct:`active`,
-    viewStyle: '/css/add-product.css',
-    docTitle:"-Add Product-"});
-});
+router.get('/add-product', getAddProduct);
 
-// POST /add-product
-router.post('/add-product', (req, res) => {
-  // Realizaremos la extracción de
-  // "name" de la peticion
-  const { title } = req.body;
-  // agregar el dato en la base de datos
-  products.push(title);
-  // redireccionando
-  res.redirect('/');
-});
+// POST /admin/add-product
+router.post('/add-product', postAddProduct);
 
 // Exportando el enrutador admin
 export default router;
-
-
